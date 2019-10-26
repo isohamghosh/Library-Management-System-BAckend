58b6f3b3bccdfbc74e19162a38d8fbbf6415f984
TejaswiniHarish <56918657+TejaswiniHarish@users.noreply.github.com>
2019-10-26 17:33:37 +0530

    changed in AdminController and LibrarianController

--- src/main/java/com/capgemini/librarymanagement/controller/AdminController.java
+++ src/main/java/com/capgemini/librarymanagement/controller/AdminController.java
@@ -1,7 +1,6 @@
 package com.capgemini.librarymanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.capgemini.librarymanagement.dto.UserResponse;
 import com.capgemini.librarymanagement.dto.Users;
 import com.capgemini.librarymanagement.services.AdminService;
 
@@ -21,68 +18,25 @@ import com.capgemini.librarymanagement.services.AdminService;
 public class AdminController {
 	@Autowired
 	private AdminService service;
-
 	
 	@PostMapping("/addLibrarian")
-	public UserResponse  addLibrarian(@RequestBody Users librarian) {
-		UserResponse response=new UserResponse();
-		
-		if(service.addLibrarian(librarian) != null) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-			response.setDescription("Librarian added Sucessfully");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-			response.setDescription("Librarian not added");
-		}return response;
-
+	public Users addLibrarian(@RequestBody Users librarian) {
+		return service.addLibrarian(librarian); 
 	}//end of addLibrarian
 	
 	@PutMapping("/updateLibrarian")
-	public UserResponse updateLibrarian(@RequestBody Users librarian) {
-		UserResponse response=new UserResponse();
-		if(service.updateLibrarian(librarian) != null) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-			response.setDescription("Librarian Updated Sucessfull");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-			response.setDescription("Librarian not updated");
-		}return response;
+	public Users updateLibrarian(@RequestBody Users librarian) {
+		return service.updateLibrarian(librarian);
 	}//end of updateLibrarian
 	
 	@GetMapping("/searchLibrarian/{id}")
-	public UserResponse searchLibrarian(@PathVariable(name = "id")String librarianId) {
-		UserResponse response=new UserResponse();
-		try {
-		Users librarians = service.searchLibrarian(librarianId);
-			if(librarians!=null) {
-				response.setStatusCode(201);
-				response.setMessage("success");
-				response.setLibrarians(librarians);
-			}else {
-				response.setStatusCode(404);
-				response.setMessage("failed");
-			}
-		}catch (Exception e) {
-			//System.out.println("");
-		}return response;
-
+	public Users searchLibrarian(@PathVariable(name = "id")String librarianId) {
+		return service.searchLibrarian(librarianId);
 	}//end of searchLibrarian
 	
 	@DeleteMapping("/deleteLibrarian/{id}")
-	public UserResponse deleteLibrarian(@PathVariable(name="id")String librarianId) {
-		UserResponse response=new UserResponse();
-		if(service.deleteLibrarian(librarianId)) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-		}return response;
-
+	public boolean deleteLibrarian(@PathVariable(name="id")String librarianId) {
+		return service.deleteLibrarian(librarianId);
 	}//end of deleteLibrarian
 
 }
--- src/main/java/com/capgemini/librarymanagement/controller/LibrarianController.java
+++ src/main/java/com/capgemini/librarymanagement/controller/LibrarianController.java
@@ -3,22 +3,18 @@ package com.capgemini.librarymanagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.librarymanagement.dto.BooksInventory;
 import com.capgemini.librarymanagement.dto.BooksRegistration;
 import com.capgemini.librarymanagement.dto.BooksTransaction;
-import com.capgemini.librarymanagement.dto.UserResponse;
 import com.capgemini.librarymanagement.dto.Users;
-import com.capgemini.librarymanagement.exception.CustomException;
 import com.capgemini.librarymanagement.services.LibrarianService;
 
 @RestController
@@ -26,90 +22,40 @@ import com.capgemini.librarymanagement.services.LibrarianService;
 public class LibrarianController {
 	@Autowired
 	private LibrarianService service;
-	
+
 	@PostMapping("/addNewBook")
-	public UserResponse addNewBook(@RequestBody BooksInventory booksInventory) {
-		UserResponse response=new UserResponse();
-		if(service.addNewBook(booksInventory) != null) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-		}return response;
-
+	public BooksInventory addNewBook(@RequestBody BooksInventory booksInventory) {
+		return service.addNewBook(booksInventory); 
 	}//end of addNewBook
-	
-	@PostMapping("/updateBook")
-	public UserResponse updateBook(@RequestBody BooksInventory booksInventory) {
-		UserResponse response=new UserResponse();
-		if(service.updateBook(booksInventory) != null) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-		}return response;
-	}//end of updateBook
-	
-	@DeleteMapping("/deleteBook/{bookId}")
-	public UserResponse deleteBook(@PathVariable(name="bookId") String bookId) {
-		UserResponse response=new UserResponse();
-		if(service.deleteBook(bookId)) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-		}return response;
 
+	@PostMapping("/updateBook")
+	public BooksInventory updateBook(@RequestBody BooksInventory booksInventory) {
+		return service.updateBook(booksInventory);
+	}//end of updateBook
+
+	@DeleteMapping("/deleteBook/{bookId}")
+	public boolean deleteBook(@PathVariable(name="bookId") String bookId) {
+		return service.deleteBook(bookId); 
 	}//end of deleteBook
-	
+
 	@GetMapping("/getAllBookRequest")
 	public List<BooksRegistration> getBookRequest() {
 		return service.getBookRequest();
 	}
-	
+
 	@PostMapping("/responseBookRequest/{id}")
 	public BooksTransaction responseBookRequest(@PathVariable(name="id") Integer id) {
 		return service.responseBookRequest(id);
 
 	}
-	
+
 	@GetMapping("/cancelBookRequest/{id}")
 	public boolean cancelBookRequest(@PathVariable(name="id") Integer id) {
 		return service.cancelBookRequest(id);
 	}
-	
+
 	@GetMapping("/searchStudent/{id}")
-	public UserResponse searchStudent(@PathVariable(name="id") String id) {
-		UserResponse response=new UserResponse();
-		try {
-		List<Users> student = service.searchStudent(id);
-			if(student!=null) {
-				response.setStatusCode(201);
-				response.setMessage("success");
-			}else {
-				response.setStatusCode(404);
-				response.setMessage("failed");
-			}
-		}catch (Exception e) {
-			throw new CustomException("No such Student Found");
-		}return response;
-
-	}//end of searchStudents
-		
-	@DeleteMapping("/deleteStudent/{id}")
-	public UserResponse deleteStudent(@PathVariable(name="id") String id) {
-		UserResponse response=new UserResponse();
-		if(service.deleteStudent(id)) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-		}return response;
-
+	public List<Users> searchStudent(@PathVariable(name="id") String id) {
+		return service.searchStudent(id);
 	}//end of deleteStudent	
-
 }
